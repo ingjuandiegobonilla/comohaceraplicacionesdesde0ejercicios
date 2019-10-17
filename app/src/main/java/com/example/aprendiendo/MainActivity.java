9c43f066143464e3bd9b13042291419aad3db541
zartos <daikerumaru@hotmail.com>
2019-10-17 15:22:19 -0500

    Reproducir sonido con SoundPool

--- app/src/main/java/com/example/aprendiendo/MainActivity.java
+++ app/src/main/java/com/example/aprendiendo/MainActivity.java
@@ -2,15 +2,19 @@ package com.example.aprendiendo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity  {
 
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private SoundPool soundPool;
+    private int soundID;
+    boolean loaded = false;
 
 
     @Override
@@ -18,7 +22,35 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        View view = findViewById(R.id.tv);
+        view.setOnTouchListener(this);
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        soundPool = new SoundPool(10,AudioManager.STREAM_MUSIC,0);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
+            }
+        });
+        soundID = soundPool.load(this,R.raw.audio,1);
+
     }
 
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            float actualVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float volume = actualVolume/maxVolume;
+
+            if(loaded){
+                soundPool.play(soundID, volume, volume, 1, 0, 1f);
+                Log.e("Test","Played sound");
+            }
+         }
+        return false;
+    }
 }

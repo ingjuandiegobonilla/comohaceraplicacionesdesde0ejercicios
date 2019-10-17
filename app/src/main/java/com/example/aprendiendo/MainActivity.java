9f5c4abe2b13bad81df97caf2461a8aabc590c49
zartos <daikerumaru@hotmail.com>
2019-10-17 14:50:13 -0500

    Next

--- app/src/main/java/com/example/aprendiendo/MainActivity.java
+++ app/src/main/java/com/example/aprendiendo/MainActivity.java
@@ -9,10 +9,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    ImageView monkey;
-    Button rotar;
+public class MainActivity extends AppCompatActivity  {
 
 
 
@@ -20,25 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        monkey = findViewById(R.id.monkey);
-        rotar = findViewById(R.id.rotateButton);
-        rotar.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()){
-
-            case R.id.rotateButton:
-                Animation rotacion;
-                rotacion = AnimationUtils.loadAnimation(this,R.anim.rotate);
-                rotacion.reset();
-                monkey.startAnimation(rotacion);
-            break;
-            default:
-                break;
-        }
 
     }
+
+
 }

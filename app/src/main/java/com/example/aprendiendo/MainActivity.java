fb75420a786011e8db39b860065197636d2f8267
zartos <daikerumaru@hotmail.com>
2019-10-19 12:08:07 -0500

    Autocompletar desde xml

--- app/src/main/java/com/example/aprendiendo/MainActivity.java
+++ app/src/main/java/com/example/aprendiendo/MainActivity.java
@@ -9,13 +9,13 @@ import android.widget.AutoCompleteTextView;
 public class MainActivity extends AppCompatActivity  {
 
     AutoCompleteTextView monthsSearch;
-    String[] months = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String[] months = getResources().getStringArray(R.array.meses);
         monthsSearch = findViewById(R.id.autocomplete_months);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,months);
         monthsSearch.setAdapter(adapter);

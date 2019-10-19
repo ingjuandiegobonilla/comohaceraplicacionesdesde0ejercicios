faba3900d0f5ce142bf66dc34b9e60dc7b80639a
zartos <daikerumaru@hotmail.com>
2019-10-19 11:55:52 -0500

    Autocompletar con un array

--- app/src/main/java/com/example/aprendiendo/MainActivity.java
+++ app/src/main/java/com/example/aprendiendo/MainActivity.java
@@ -3,21 +3,22 @@ package com.example.aprendiendo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity  {
 
-
+    AutoCompleteTextView monthsSearch;
+    String[] months = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        monthsSearch = findViewById(R.id.autocomplete_months);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,months);
+        monthsSearch.setAdapter(adapter);
     }
 
 

72d24e64c0482a8799bb8864e7838ff96b73b756
zartos <daikerumaru@hotmail.com>
2019-10-29 14:04:59 -0500

    new Threads

--- app/src/main/java/com/example/aprendiendo/MainActivity.java
+++ app/src/main/java/com/example/aprendiendo/MainActivity.java
@@ -8,17 +8,48 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    Button runThread;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        runThread = findViewById(R.id.run_thread);
+        runThread.setOnClickListener(this);
+        i = 0;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (i = 0; i < 10000; i++) {
+                    System.out.println(i);
+
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.run_thread:
+                String valor = String.valueOf(i);
+                Toast.makeText(MainActivity.this, valor, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+    }
 }

1cddb6d3a2020702a348941958ff5f429ede63b7
zartos <daikerumaru@hotmail.com>
2019-10-29 15:01:01 -0500

    Call working

--- app/src/main/java/com/example/aprendiendo/MainActivity.java
+++ app/src/main/java/com/example/aprendiendo/MainActivity.java
@@ -2,23 +2,49 @@ package com.example.aprendiendo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button llamar;
+    EditText numero;
+    String numeroUsuario;
 
+    private void llamar(String numero){
+        Uri uri = Uri.parse("tel:" + numero);
+        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        llamar = findViewById(R.id.call);
+        numero = findViewById(R.id.phone_number);
+        llamar.setOnClickListener(this);
+
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.call:
+                numeroUsuario = numero.getText().toString();
+                llamar(numeroUsuario);
+                break;
+            default:
+                break;
+        }
+    }
 }
